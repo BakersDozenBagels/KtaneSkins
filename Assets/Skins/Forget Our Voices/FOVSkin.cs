5a2d4ef2835aef7958a7a9ed99e0b0ba045dbfe3
BakersDozenBagels <bakersdozenbagelsbergman@gmail.com>
2025-07-27 16:36:52 -0400

    [V1.5] Remove the voice of a transphobe

---
+++ Assets/Skins/Forget Our Voices/FOVSkin.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+public class FOVSkin : ModuleSkin
+{
+    public override string ModuleId { get { return "forgetOurVoices"; } }
+    public override string Name { get { return "TransRightsAreHumanRights"; } }
+
+    protected override Dictionary<string, string> SoundOverrides
+    {
+        get
+        {
+            var overrides = new Dictionary<string, string>() {
+                { "Solve_MQE", "ForgetOurVoices_Solve" },
+                { "Strike_MQE", "ForgetOurVoices_Strike" },
+            };
+            for (int i = 0; i < 10; i++)
+                overrides["M" + i] = "ForgetOurVoices_" + i;
+
+            return overrides;
+        }
+    }
+}
